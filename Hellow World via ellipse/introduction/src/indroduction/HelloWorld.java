022d823f6acb7e5223b48cd131e310864477d5fd
Aman Jain <amanforcode@gmail.com>
2019-08-08 00:48:47 +0530

    My first java code.

---
+++ Hellow World via ellipse/introduction/src/indroduction/HelloWorld.java
@@ -0,0 +1,10 @@
+package indroduction;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World ! Eclipse Downloaded!");
+	}
+
+}
